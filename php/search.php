9af5a75432b57eadee30f8b85acb6ecba1af5bcc
Sazidul Alam <81089575+SazidulAlam47@users.noreply.github.com>
2021-04-12 17:44:43 +0600

    Delete php directory

--- php/search.php
+++
@@ -1,9 +0,0 @@
-<?php
-    extract($_REQUEST);
-    $file=fopen("search-save.txt","a");
-
-    fwrite($file,"Search :");
-    fwrite($file, $search ."\n\n");
-    fclose($file);
-    header("location: ../index.html");
- ?>
